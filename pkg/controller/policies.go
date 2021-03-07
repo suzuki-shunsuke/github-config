1203649faaf813bcb52ed71ac842b38d629306ea
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-03-07 15:37:50 +0900

    feat: add repo policy has_issues

--- pkg/controller/policies.go
+++ pkg/controller/policies.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/suzuki-shunsuke/github-config/pkg/domain"
+	"github.com/suzuki-shunsuke/github-config/pkg/rule/repo/hasissues"
 	"github.com/suzuki-shunsuke/github-config/pkg/rule/repo/hasprojects"
 )
 
@@ -10,5 +11,6 @@ type NewRepoPolicy func(param map[string]interface{}, action domain.ActionConfig
 func supportedRepoPolicies() map[string]NewRepoPolicy {
 	return map[string]NewRepoPolicy{
 		"has_projects": hasprojects.New,
+		"has_issues":   hasissues.New,
 	}
 }
